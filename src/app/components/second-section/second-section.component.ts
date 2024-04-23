25843673e59ee788a050c73b9f362c18c9e182eb
Nick <nick@nickvitulli.dev>
2024-04-22 23:35:53 -0400

    added bootstrap, updated styling for whoami, created nav bar, started a card component

--- src/app/components/second-section/second-section.component.ts
+++ src/app/components/second-section/second-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WhoamiComponent } from '../whoami/whoami.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-second-section',
   standalone: true,
-  imports: [WhoamiComponent],
+  imports: [WhoamiComponent, NavbarComponent],
   templateUrl: './second-section.component.html',
   styleUrl: './second-section.component.scss',
 })
--- src/app/components/whoami/whoami.component.ts
+++ src/app/components/whoami/whoami.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CardComponent } from '../card/card.component';
 @Component({
   selector: 'app-whoami',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './whoami.component.html',
-  styleUrl: './whoami.component.scss'
+  styleUrl: './whoami.component.scss',
 })
-export class WhoamiComponent {
-
-}
+export class WhoamiComponent {}
