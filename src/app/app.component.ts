7f69275a4bc5ccdc3ec99d7425bb5051e2edcc23
Nick <nick@nickvitulli.dev>
2024-05-03 20:12:15 -0400

    added footer copyright

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstSectionComponent } from './components/first-section/first-section.component';
 import { SecondSectionComponent } from './components/second-section/second-section.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstSectionComponent, SecondSectionComponent],
+  imports: [
+    RouterOutlet,
+    FirstSectionComponent,
+    SecondSectionComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
